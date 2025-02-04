b79abc127a9107cf0125f0da5c4853ac96586700
DODO <dohae.kim22@gmail.com>
2025-02-04 17:55:52 +0100

    Add Search Bar Animation

--- js/main.js
+++ js/main.js
@@ -21,10 +21,12 @@ cartEl.addEventListener("click", (event) => {
 
 // Search Bar
 const headerEl = document.querySelector("header");
+const menuListEls = [...headerEl.querySelectorAll(".menu > li")];
 const searchStarterEl = headerEl.querySelector(".search-starter");
 const searchWrapEl = headerEl.querySelector(".search-wrap");
 const searchCloserEl = searchWrapEl.querySelector(".search-closer");
 const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
 searchStarterEl.addEventListener("click", addSearching);
 
@@ -35,9 +37,22 @@ searchShadowEl.addEventListener("click", removeSearching);
 function addSearching() {
   headerEl.classList.add("searching");
   document.documentElement.classList.add("fixed");
+  menuListEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / menuListEls.length + "s";
+  });
+  searchDelayEls.forEach((el, i) => {
+    el.style.transitionDelay = (i * 1) / searchDelayEls.length + "s";
+  });
 }
 
 function removeSearching() {
   headerEl.classList.remove("searching");
   document.documentElement.classList.remove("fixed");
+  menuListEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / menuListEls.length + "s";
+  });
+  searchDelayEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = (i * 0.4) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
 }
